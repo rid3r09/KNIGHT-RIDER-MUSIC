2eb4daf30629b1c77b195a88cc14ecd46846bd95
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2023-05-14 13:34:40 +0530

    Update __main__.py

--- VipX/__main__.py
+++ VipX/__main__.py
@@ -67,7 +67,7 @@ async def init():
     except:
         pass
     await Vip.decorators()
-    LOGGER("VipX").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝗩𝗜𝗣 𝗕𝗢𝗬♨️\n╚═════ஜ۩۞۩ஜ════╝")
+    LOGGER("VipX").info("╔═════ஜ۩۞۩ஜ════╗\n  ♨️𝗠𝗔𝗗𝗘 𝗕𝗬 𝐆𝐎𝐃𝐅𝐀𝐓𝐇𝐄𝐑♨️\n╚═════ஜ۩۞۩ஜ════╝")
     await idle()
 
 

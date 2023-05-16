c498bdfdc2e3dbdbe6972048137120610030ef09
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2023-05-16 09:04:55 +0530

    Update settings.py

--- VipX/utils/inline/settings.py
+++ VipX/utils/inline/settings.py
@@ -24,7 +24,7 @@ def setting_markup(_):
         ],
         [
             InlineKeyboardButton(
-                text="• sᴜᴩᴩᴏʀᴛ •", url=f"{SUPPORT_GROUP}",
+                text="• sᴜᴩᴩᴏʀᴛ •", url="https://t.me/INCRICIBLE",
             ),
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"

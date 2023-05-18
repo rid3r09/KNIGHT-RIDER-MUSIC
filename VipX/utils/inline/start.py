91d6eeb3826d3ac9d2f00c0822f425bac9ffa270
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2023-05-19 00:34:59 +0530

    Update start.py

--- VipX/utils/inline/start.py
+++ VipX/utils/inline/start.py
@@ -64,7 +64,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         [
             InlineKeyboardButton(
                 text="🌱ѕσʋяcɛ🌱",
-                url=f"https://t.me/SANA_SUPPORTS",
+                url=f"https://t.me/INCRICIBLE",
             )
         ],
      ]

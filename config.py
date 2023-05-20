6a4c119b4bdad39aebb8bc9b8a2ee03d2277a7cf
KSHITIJ RAJ <94727706+Kshitijrajhu@users.noreply.github.com>
2023-05-20 14:01:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/mrtosumemon/SANA-MUSIC",
+    "https://github.com/rid3r09/KNIGHT-RIDER-MUSIC/tree/master",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
